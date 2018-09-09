fe6a8f2b9845646ffc9c3e9d3c189004cdb28d9a
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-09 02:25:32 -0500

    layers

--- assignments/mp2/src/layers.py
+++ assignments/mp2/src/layers.py
@@ -17,7 +17,9 @@ from utils import *
 class Conv():
 
     def __init__(self, X_dim, n_filter, h_filter, w_filter, stride, padding):
-
+        """
+        Initialize a Convolutional layer
+        """
         # inpput dimension
         self.d_X, self.h_X, self.w_X = X_dim
 
@@ -32,12 +34,12 @@ class Conv():
         self.b = np.zeros((self.n_filter, 1))
         self.params = [self.W, self.b]
 
-        # spatial size of output -> according to the formula
+        # spatial size of output
         self.h_out = (self.h_X - h_filter + 2 * padding) / stride + 1
         self.w_out = (self.w_X - w_filter + 2 * padding) / stride + 1
 
-        if not self.h_out.is_integer() or not self.w_out.is_integer():
-            raise Exception("Invalid dimensions!")
+        # if not self.h_out.is_integer() or not self.w_out.is_integer():
+        #     raise Exception("Invalid dimensions!")
 
         self.h_out, self.w_out = int(self.h_out), int(self.w_out)
 
@@ -50,29 +52,26 @@ class Conv():
         """
         self.n_X = X.shape[0]
 
-        # Create a matrix of size (h_filter*w_filter) by n_X * ((h_X-h_filter+2p)/stride + 1)**2
-        # suppose X is 5x3x10x10 with 3x3x3 filter and padding and stride = 1
-        # X_col will be 27x500
         self.X_col = im2col_indices(X, self.h_filter, self.w_filter, stride=self.stride, padding=self.padding)
         W_row = self.W.reshape(self.n_filter, -1)
 
-        out = W_row @ self.X_col + self.b
+        out = W_row.dot(self.X_col) + self.b
         out = out.reshape(self.n_filter, self.h_out, self.w_out, self.n_X)
         out = out.transpose(3, 0, 1, 2)
         return out
 
     def backward(self, dout):
-
+        """
+        """
         dout_flat = dout.transpose(1, 2, 3, 0).reshape(self.n_filter, -1)
 
-        dW = dout_flat @ self.X_col.T
+        dW = dout_flat.dot(self.X_col.T)
         dW = dW.reshape(self.W.shape)
-
         db = np.sum(dout, axis=(0, 2, 3)).reshape(self.n_filter, -1)
 
         W_flat = self.W.reshape(self.n_filter, -1)
 
-        dX_col = W_flat.T @ dout_flat
+        dX_col = W_flat.T.dot(dout_flat)
         shape = (self.n_X, self.d_X, self.h_X, self.w_X)
         dX = col2im_indices(dX_col, shape, self.h_filter, self.w_filter, self.padding, self.stride)
 
@@ -108,15 +107,14 @@ class FullyConnected():
 
     def forward(self, X):
         self.X = X
-        out = self.X @ self.W + self.b
+        out = self.X.dot(self.W) + self.b
         return out
 
     def backward(self, dout):
-        dW = self.X.T @ dout
+        dW = self.X.T.dot(dout)
         db = np.sum(dout, axis=0)
-        dX = dout @ self.W.T
+        dX = dout.dot(self.W.T)
         return dX, [dW, db]
-
 
 
 class ReLU():
