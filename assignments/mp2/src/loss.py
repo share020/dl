da78d8e7de4085bed9ca70c0e54fae9ef6ff4655
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-09 02:25:43 -0500

    loss

--- assignments/mp2/src/loss.py
+++ assignments/mp2/src/loss.py
@@ -18,28 +18,17 @@ def delta_l2_regularization(layers, grads, lam=0.001):
     return grads
 
 
-# def l1_regularization(layers, lam=0.001):
-#     reg_loss = 0.0
-#     for layer in layers:
-#         if hasattr(layer, 'W'):
-#             reg_loss += lam * np.sum(np.abs(layer.W))
-#     return reg_loss
-#
-#
-# def delta_l1_regularization(layers, grads, lam=0.001):
-#     for layer, grad in zip(layers, reversed(grads)):
-#         if hasattr(layer, 'W'):
-#             grad[0] += lam * layer.W / (np.abs(layer.W) + 1e-8)
-#     return grads
-
-
 def SoftmaxLoss(X, y):
+    """
+    softmax loss
+    """
     m = y.shape[0]
     p = softmax(X)
-    log_likelihood = -np.log(p[range(m), y] + np.finfo)
+    log_likelihood = -np.log(p[range(m), y])
     loss = np.sum(log_likelihood) / m
 
-    dx = p.copy()
+    # dx = p.copy()
+    dx = p
     dx[range(m), y] -= 1
     dx /= m
     return loss, dx
