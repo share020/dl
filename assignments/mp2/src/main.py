7a05bc87a819e0be068c3a709e6dccd8eedbe063
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-09 02:25:54 -0500

    pipeline

--- assignments/mp2/src/main.py
+++ assignments/mp2/src/main.py
@@ -27,37 +27,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dataroot', type=str, default="../MNISTdata.hdf5", help='path to dataset')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
 parser.add_argument('--epochs', type=int, default=50, help='number of epochs to train')
-# parser.add_argument('--n_x', type=int, default=784, help='number of inputs')
-# parser.add_argument('--n_h', type=int, default=64, help='number of hidden units')
-# parser.add_argument('--beta', type=float, default=0.9, help='parameter for momentum')
 parser.add_argument('--minibatch_size', type=int, default=64, help='input batch size')
 
 
 # filter parameters
 parser.add_argument('--n_filter', type=int, default=32, help='number of filters')
-parser.add_argument('--h_filter', type=int, default=3, help='height of filters')
-parser.add_argument('--w_filter', type=int, default=3, help='width of filters')
+parser.add_argument('--h_filter', type=int, default=7, help='height of filters')
+parser.add_argument('--w_filter', type=int, default=7, help='width of filters')
 parser.add_argument('--stride', type=int, default=1, help='stride')
 parser.add_argument('--padding', type=int, default=1, help='zero padding')
 
+
 # dataset parameters mnist_dims
 parser.add_argument('--num_class', type=int, default=10, help='number of classes in MNIST dataset')
-
-
-# mnist_dims, num_class=10, n_filter=opt.n_filter, h_filter=opt.h_filter, w_filter=opt.w_filter, stride=opt.stride, padding=opt.padding, loss_func=SoftmaxLoss
+parser.add_argument('--num_channel', type=int, default=1, help='number of channels in MNIST dataset')
+parser.add_argument('--img_height', type=int, default=28, help='height of images in MNIST dataset')
+parser.add_argument('--img_width', type=int, default=28, help='width of images in MNIST dataset')
 
 
 # parse the arguments
 opt = parser.parse_args()
-
-
-# def build_layers(X_dim, num_class):
-#     conv = Conv(X_dim, n_filter=opt.n_filter, h_filter=opt.h_filter, w_filter=opt.w_filter, stride=opt.stride, padding=opt.padding)
-#     relu_conv = ReLU()
-#     flat = Flatten()
-#     fc = FullyConnected(np.prod(conv.out_dim), num_class)
-#     return [conv, relu_conv, flat, fc]
-
 
 
 if __name__ == '__main__':
@@ -65,29 +54,25 @@ if __name__ == '__main__':
     pipepline of convolution neural network from scratch in Python for the MNIST dataset
     """
     # load dataset
-    training_set, test_set = mnist_loader(opt.dataroot)
+    training_set, test_set = mnist_loader(opt.dataroot, one_hot=False, debug=True)
     X, y = training_set
     X_test, y_test = test_set
 
     # mnist image dims
-    mnist_dims = (1, 28, 28)
+    mnist_dims = (opt.num_channel, opt.img_height, opt.img_width)
 
-    # build layers
-    # layers = build_layers(mnist_dims, num_class=10)
-    # # create cnn class
-    # cnn = CNN(layers)
-    # cnn = sgd_momentum(cnn, X, y, minibatch_size=35, epoch=20,
-    #                    learning_rate=0.01, X_test=X_test, y_test=y_test)
-
+    print(">>> Initialize CNN model ...")
     # create cnn class
     cnn = CNN(mnist_dims,
-              num_class=10,
+              num_class=opt.num_class,
               n_filter=opt.n_filter,
               h_filter=opt.h_filter,
               w_filter=opt.w_filter,
               stride=opt.stride,
               padding=opt.padding)
+    print(">>> Initialize CNN model Done...")
 
+    print(">>> Initialize GradientDescentOptimizer ...")
     # create GradientDescentOptimizer
     optim = GradientDescentOptimizer(cnn,
                                      X, y,
@@ -97,5 +82,10 @@ if __name__ == '__main__':
                                      X_test=X_test,
                                      y_test=y_test)
 
+    print(">>> Training ...")
     # minimize loss
     optim.minimize()
+
+
+    # sgd(cnn, X, y, opt.minibatch_size, opt.epochs, opt.lr, True,
+    #         X_test, y_test)
