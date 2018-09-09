8b1e3eaafb29e0084caf0ce97e773da4c277a787
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-09 02:59:11 -0500

    optimizer

--- assignments/mp2/src/optim.py
+++ assignments/mp2/src/optim.py
@@ -1,18 +1,14 @@
-def sgd(nnet, X_train, y_train, minibatch_size, epoch, learning_rate, verbose=True, X_test=None, y_test=None):
-    minibatches = get_minibatches(X_train, y_train, minibatch_size)
-    for i in range(epoch):
-        loss = 0
-        if verbose:
-            print("Epoch {0}".format(i + 1))
-        for X_mini, y_mini in minibatches:
-            loss, grads = nnet.train_step(X_mini, y_mini)
-            vanilla_update(nnet.params, grads, learning_rate=learning_rate)
-        if verbose:
-            train_acc = accuracy(y_train, nnet.predict(X_train))
-            test_acc = accuracy(y_test, nnet.predict(X_test))
-            print("Loss = {0} | Training Accuracy = {1} | Test Accuracy = {2}".format(
-                loss, train_acc, test_acc))
-    return nnet
+"""
+HW2: Implement and train a convolution neural network from scratch in Python for the MNIST dataset (no PyTorch).
+
+The convolution network should have a single hidden layer with multiple channels.
+
+Due September 14 at 5:00 PM.
+
+@author: Zhenye Na
+@date: Sep 9
+"""
+
 
 import numpy as np
 from sklearn.utils import shuffle
@@ -24,13 +20,31 @@ from loss import *
 
 
 class GradientDescentOptimizer(object):
-    """docstring for SGDOptimizer."""
-    def __init__(self, nnet, X_train, y_train, minibatch_size, epochs, learning_rate, verbose=True, X_test=None, y_test=None):
+    """Stochastic Gradient Descent Optimizer."""
+    def __init__(self, nnet, X_train, y_train, minibatch_size, epochs,
+                 learning_rate, verbose=True, X_test=None, y_test=None):
+        """
+        Stochastic Gradient Descent Optimizer.
+
+        inputs
+            nnet
+            X_train
+            y_train
+            minibatch_size
+            epochs
+            learning_rate
+            verbose
+            X_test
+            y_test
+        """
+        # neural net model
         self.nnet = nnet
 
+        # training set
         self.X_train = X_train
         self.y_train = y_train
 
+        # hyper-parameters
         self.minibatch_size = minibatch_size
         self.epochs = epochs
         self.learning_rate = learning_rate
@@ -46,30 +60,29 @@ class GradientDescentOptimizer(object):
         """
         minimize loss
         """
-
+        # permute minibatches
         minibatches = self.get_minibatches()
 
         for i in range(self.epochs):
-            loss = 0
 
+            # training and update params
             for X_mini, y_mini in minibatches:
                 loss, grads = self.nnet.train_step(X_mini, y_mini)
-                self.vanilla_update(grads)
+                self.update_params(grads)
 
             if self.verbose:
-                # print("Epoch {0}".format(i + 1))
-                train_acc = accuracy(self.y_train, self.nnet.predict(self.X_train))
-                test_acc = accuracy(self.y_test, self.nnet.predict(self.X_test))
-                print("Epoch {0} | Loss = {1} | Training Accuracy = {2} | Test Accuracy = {3}".format(i + 1, loss, train_acc, test_acc))
+                train_acc = self.accuracy(self.y_train, self.nnet.predict(self.X_train))
+                test_acc  = self.accuracy(self.y_test, self.nnet.predict(self.X_test))
+                print("Epoch {0}, Loss = {1}, Training Accuracy = {2}, Test Accuracy = {3}".format(i + 1, loss, train_acc, test_acc))
 
     def get_minibatches(self, isShuffle=True):
+        """
+        get mini batches
+        """
         m = self.X_train.shape[0]
         minibatches = []
 
-        print(self.X_train.shape)
-        print(self.y_train.shape)
-
-        X, y = self.X_train, self.y_train
+        X, y = self.X_train.copy(), self.y_train.copy()
 
         if isShuffle:
             X, y = shuffle(X, y)
@@ -78,12 +91,19 @@ class GradientDescentOptimizer(object):
             X_batch = X[i:i + self.minibatch_size, :, :, :]
             y_batch = y[i:i + self.minibatch_size, ]
             minibatches.append((X_batch, y_batch))
+
         return minibatches
 
-    def vanilla_update(self, grads):
+    def update_params(self, grads):
         """
         update parameters
         """
         for param, grad in zip(self.nnet.params, reversed(grads)):
             for i in range(len(grad)):
                 param[i] += - self.learning_rate * grad[i]
+
+    def accuracy(self, y_true, y_pred):
+        """
+        accuracy
+        """
+        return np.mean(y_pred == y_true)
