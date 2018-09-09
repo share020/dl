cc6386978bed39ed3c0f6bba78684aae5b03831d
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-08 19:31:10 -0500

    cnn model

--- assignments/mp2/src/cnn.py
+++ assignments/mp2/src/cnn.py
@@ -12,15 +12,64 @@ Due September 14 at 5:00 PM.
 
 import numpy as np
 
+from loss import *
+from utils import *
+from layers import *
+
 
 class CNN:
+    """
+    Convolutional Neural Network model
+    """
+    def __init__(self, mnist_dims, num_class, n_filter, h_filter,
+                 w_filter, stride, padding, loss_func=SoftmaxLoss):
+        """
+        Initialize a CNN model
 
-    def __init__(self, layers, loss_func=SoftmaxLoss):
-        self.layers = layers
+        inputs:
+            mnist_dims:
+            num_class:
+            n_filter:
+            h_filter:
+            w_filter:
+            stride:
+            padding:
+            loss_func:
+        """
+        # build layers
+        self.layers = self.build_layers(mnist_dims,
+                                        n_filter,
+                                        h_filter,
+                                        w_filter,
+                                        stride,
+                                        padding,
+                                        num_class)
+
+        # parameters
         self.params = []
         for layer in self.layers:
             self.params.append(layer.params)
+
+        # loss function
         self.loss_func = loss_func
+
+    def build_layers(self, mnist_dims, n_filter, h_filter, w_filter, stride, padding, num_class):
+        """
+        Build layers of CNN model
+        """
+        # Convolutional layer
+        conv = Conv(mnist_dims, n_filter, h_filter, w_filter, stride, padding)
+
+        # ReLU activation layer
+        relu_conv = ReLU()
+
+        # flatten
+        flat = Flatten()
+
+        # Fully Connected layer
+        fc = FullyConnected(np.prod(conv.out_dim), num_class)
+
+        return [conv, relu_conv, flat, fc]
 
     def forward(self, X):
         for layer in self.layers:
@@ -45,125 +94,3 @@ class CNN:
     def predict(self, X):
         X = self.forward(X)
         return np.argmax(softmax(X), axis=1)
-
-
-
-
-
-class Conv():
-
-    def __init__(self, X_dim, n_filter, h_filter, w_filter, stride, padding):
-
-        self.d_X, self.h_X, self.w_X = X_dim
-
-        self.n_filter, self.h_filter, self.w_filter = n_filter, h_filter, w_filter
-        self.stride, self.padding = stride, padding
-
-        self.W = np.random.randn(n_filter, self.d_X, h_filter, w_filter) / np.sqrt(n_filter / 2.)
-        self.b = np.zeros((self.n_filter, 1))
-        self.params = [self.W, self.b]
-
-        self.h_out = (self.h_X - h_filter + 2 * padding) / stride + 1
-        self.w_out = (self.w_X - w_filter + 2 * padding) / stride + 1
-
-        if not self.h_out.is_integer() or not self.w_out.is_integer():
-            raise Exception("Invalid dimensions!")
-
-        self.h_out, self.w_out = int(self.h_out), int(self.w_out)
-        self.out_dim = (self.n_filter, self.h_out, self.w_out)
-
-    def forward(self, X):
-
-        self.n_X = X.shape[0]
-
-        self.X_col = im2col_indices(
-            X, self.h_filter, self.w_filter, stride=self.stride, padding=self.padding)
-        W_row = self.W.reshape(self.n_filter, -1)
-
-        out = W_row @ self.X_col + self.b
-        out = out.reshape(self.n_filter, self.h_out, self.w_out, self.n_X)
-        out = out.transpose(3, 0, 1, 2)
-        return out
-
-    def backward(self, dout):
-
-        dout_flat = dout.transpose(1, 2, 3, 0).reshape(self.n_filter, -1)
-
-        dW = dout_flat @ self.X_col.T
-        dW = dW.reshape(self.W.shape)
-
-        db = np.sum(dout, axis=(0, 2, 3)).reshape(self.n_filter, -1)
-
-        W_flat = self.W.reshape(self.n_filter, -1)
-
-        dX_col = W_flat.T @ dout_flat
-        shape = (self.n_X, self.d_X, self.h_X, self.w_X)
-        dX = col2im_indices(dX_col, shape, self.h_filter,
-                            self.w_filter, self.padding, self.stride)
-
-        return dX, [dW, db]
-
-
-class Flatten():
-
-    def __init__(self):
-        self.params = []
-
-    def forward(self, X):
-        self.X_shape = X.shape
-        self.out_shape = (self.X_shape[0], -1)
-        out = X.ravel().reshape(self.out_shape)
-        self.out_shape = self.out_shape[1]
-        return out
-
-    def backward(self, dout):
-        out = dout.reshape(self.X_shape)
-        return out, ()
-
-
-class FullyConnected():
-
-    def __init__(self, in_size, out_size):
-
-        self.W = np.random.randn(in_size, out_size) / np.sqrt(in_size / 2.)
-        self.b = np.zeros((1, out_size))
-        self.params = [self.W, self.b]
-
-    def forward(self, X):
-        self.X = X
-        out = self.X @ self.W + self.b
-        return out
-
-    def backward(self, dout):
-        dW = self.X.T @ dout
-        db = np.sum(dout, axis=0)
-        dX = dout @ self.W.T
-        return dX, [dW, db]
-
-
-
-
-class ReLU():
-    def __init__(self):
-        self.params = []
-
-    def forward(self, X):
-        self.X = X
-        return np.maximum(X, 0)
-
-    def backward(self, dout):
-        dX = dout.copy()
-        dX[self.X <= 0] = 0
-        return dX, []
-
-
-class sigmoid():
-
-    def forward(self, X):
-        out = 1.0 / (1.0 + np.exp(X))
-        self.out = out
-        return out
-
-    def backward(self, dout):
-        dX = dout * self.out * (1 - self.out)
-        return dX, []
