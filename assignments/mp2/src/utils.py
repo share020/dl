c424035cd365ac751faf58364625f5681f0c6621
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-09 02:59:19 -0500

    utils

--- assignments/mp2/src/utils.py
+++ assignments/mp2/src/utils.py
@@ -13,32 +13,38 @@ import h5py
 import numpy as np
 
 
-def mnist_loader(path, one_hot=False):
+def mnist_loader(path, one_hot=False, debug=True):
     """
-    Load MNIST Dataset
+    Load MNIST Dataset.
 
     inputs:
-
+        path: str, path to dataset
     outputs:
-
+        (x_train, y_train), (x_test, y_test): training set, test set
     """
     # load MNIST data
+    print(">>> Loading MNIST dataset...")
     MNIST_data = h5py.File(path, 'r')
     x_train = np.float32(MNIST_data['x_train'][:])
-    y_train = np.int32(np.array(MNIST_data['y_train'][:, 0])).reshape(-1, 1)
+    y_train = np.int32(np.array(MNIST_data['y_train'][:, 0]))
     x_test  = np.float32(MNIST_data['x_test'][:])
-    y_test  = np.int32(np.array(MNIST_data['y_test'][:, 0])).reshape(-1, 1)
+    y_test  = np.int32(np.array(MNIST_data['y_test'][:, 0]))
     MNIST_data.close()
-
+    print(">>> Loading MNIST dataset Done!")
 
     # reshape to input shape
     shape = (-1, 1, 28, 28)
     x_train = x_train.reshape(shape)
-    x_test = x_test.reshape(shape)
+    x_test  = x_test.reshape(shape)
 
     if one_hot:
         y_train = one_hot_encode(y_train, 10)
-        y_test = one_hot_encode(y_test, 10)
+        y_test  = one_hot_encode(y_test, 10)
+
+    if debug:
+        num_training, num_test = 2500, 1000
+        x_train, y_train = x_train[range(num_training)], y_train[range(num_training)]
+        x_test, y_test = x_test[range(num_test)], y_test[range(num_test)]
 
     return (x_train, y_train), (x_test, y_test)
 
@@ -49,11 +55,6 @@ def one_hot_encode(y, num_class):
     for i in range(m):
         onehot[i][y[i]] = 1
     return onehot
-
-
-def accuracy(y_true, y_pred):
-    # both are not one hot encoded
-    return np.mean(y_pred == y_true)
 
 
 def softmax(x):
@@ -77,7 +78,7 @@ def get_im2col_indices(x_shape, field_height=3, field_width=3, padding=1, stride
     i = i0.reshape(-1, 1) + i1.reshape(1, -1)
     j = j0.reshape(-1, 1) + j1.reshape(1, -1)
 
-    k = np.repeat(np.arange(C,dtype='int32'), field_height * field_width).reshape(-1, 1)
+    k = np.repeat(np.arange(C, dtype='int32'), field_height * field_width).reshape(-1, 1)
 
     return (k, i, j)
 
@@ -103,12 +104,14 @@ def col2im_indices(cols, x_shape, field_height=3, field_width=3, padding=1, stri
     An implementation of col2im based on fancy indexing and np.add.at
     """
     N, C, H, W = x_shape
+
     H_padded, W_padded = H + 2 * padding, W + 2 * padding
     x_padded = np.zeros((N, C, H_padded, W_padded), dtype=cols.dtype)
     k, i, j = get_im2col_indices(x_shape, field_height, field_width, padding, stride)
     cols_reshaped = cols.reshape(C * field_height * field_width, -1, N)
     cols_reshaped = cols_reshaped.transpose(2, 0, 1)
     np.add.at(x_padded, (slice(None), k, i, j), cols_reshaped)
+
     if padding == 0:
         return x_padded
     return x_padded[:, :, padding:-padding, padding:-padding]
