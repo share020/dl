Fixed overflow bug in bw

assignments/mp3/src/cnn.py
@@ -32,11 +32,17 @@ class CNN(nn.Module):
 
             # Conv Layer block 2
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             # Conv Layer block 3
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
@@ -45,9 +51,9 @@ class CNN(nn.Module):
         self.fc_layer = nn.Sequential(
             nn.Linear(4096, 1024),
             nn.ReLU(inplace=True),
-            nn.Linear(1024, 256),
+            nn.Linear(1024, 512),
             nn.ReLU(inplace=True),
-            nn.Linear(256, 10)
+            nn.Linear(512, 10)
         )
 
 

assignments/mp3/src/main.py
@@ -43,7 +43,7 @@ parser.add_argument('--batch_size_test', type=int, default=64, help='test set in
 
 # training settings
 parser.add_argument('--resume', type=bool, default=False, help='whether training from ckpt')
-parser.add_argument('--is_gpu', type=bool, default=False, help='whether training using GPU')
+parser.add_argument('--is_gpu', type=bool, default=True, help='whether training using GPU')
 
 # parse the arguments
 opt = parser.parse_args()
@@ -115,7 +115,7 @@ if opt.is_gpu:
 
 # Loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.RMSprop(net.parameters(), lr=opt.lr, weight_decay=opt.wd)
+optimizer = optim.Adam(net.parameters(), lr=opt.lr, weight_decay=opt.wd)
 
 
 def calculate_accuracy(loader, is_gpu):
@@ -167,7 +167,15 @@ for epoch in range(start_epoch, opt.epochs + start_epoch):
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
+
+        if epoch > 16:
+            for group in optimizer.param_groups:
+                for p in group['params']:
+                    state = optimizer.state[p]
+                    if state['step'] >= 1024:
+                        state['step'] = 1000
         optimizer.step()
+
 
         # print statistics
         running_loss += loss.data[0]