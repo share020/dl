cfe099c39f11c8d335b882ba137a1bb8cb64d149
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-22 16:47:37 -0500

    mp4 initial commit

---
+++ assignments/mp4/README.md
@@ -0,0 +1,3 @@
+# HW4: Implement a deep residual neural network for CIFAR100.
+
+> Use data augmentation techniques and dropout. Due September 28.
---
+++ assignments/mp4/src/utils.py
@@ -0,0 +1,9 @@
+"""
+HW4: Implement a deep residual neural network for CIFAR100.
+
+Use data augmentation techniques and dropout.
+
+Due September 28.
+
+@author: Zhenye Na
+"""
