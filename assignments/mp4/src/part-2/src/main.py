e2ee62ab0437c9bdcb1a1f2a6624b7f4e4765832
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-02 07:35:59 -0500

    hw4 part-2

--- assignments/mp4/src/part-2/src/main.py
+++ assignments/mp4/src/part-2/src/main.py
@@ -10,20 +10,21 @@ Due October 5 at 5:00 PM.
 
 import torch
 import torchvision
-
+import torchvision.transforms as transforms
 
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth'
 }
 
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+
 def resnet18(pretrained = True) :
-    
+
     model = torchvision.models.resnet.ResNet(torchvision.models.resnet.BasicBlock, [2,2,2,2])
-    
+
     if pretrained:
         model.load_state_dict(torch.utils.model_zoo.load_url(model_urls['resnet18'], model_dir = './'))
     return model
-
-
-
-
