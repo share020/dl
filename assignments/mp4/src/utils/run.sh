e61fa924ab0d0b1390aacc141363ab7da793b5a3
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-09 23:34:21 -0500

    update shell scripts

--- assignments/mp4/src/utils/run.sh
+++ assignments/mp4/src/utils/run.sh
@@ -4,14 +4,14 @@
 
 # MODIFY THESE
 declare training_file="main.py"
-declare walltime="01:00:00"
-declare jobname="bluewaters_test"
+declare walltime="06:00:00"
+declare jobname="cs598-mp4"
 declare netid="yournetid"
 declare -a directory=("~/scratch/part-1/src/" "~/scratch/part-2/src/")
 
 for job in "${directory[@]}"
 do
-    python gen_pbs.py $training_file $walltime $jobname $netid $directory $trial > run.pbs
+    python gen_pbs.py $training_file $walltime $jobname $netid $directory $job > run.pbs
     echo "Submitting $job"
-    # qsub run.pbs -A bauh
+    qsub run.pbs -A bauh
 done
